79ec31ef89a434d38ad717b40c92b204605bdc94
scotch115 <scotch115@users.noreply.github.com>
2019-10-16 13:11:33 -0400

    Added some new JS files

---
+++ public/index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const server = express()
+
+const port = process.argv[2] || 8080;
+
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css'
+};
+
+http.createServer(function (req, res) {
+  console.log('${req.method} ${req.url}');
+  
+  const parsedUrl = url.parse(req.url);
+  
+  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+//  let pathname = path.join(__dirname, sanitizePath);
+    var pathname = './landing.html';
+  
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      res.statusCode = 404;
+      res.end('File ${pathname} not found!');
+      return;
+    }
+    
+    if (fs.statSync(pathname).isDirectory()) {
+    pathname = './${pathname}';
+    }
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        const ext = path.parse(pathname).ext;
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+}).listen(parseInt(port));
+
+console.log('Server Listening on port 8080!');
