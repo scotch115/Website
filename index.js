08ae390d1769862d482c0256e63b846f87a5e8c6
scotch115 <jordangamache115@gmail.com>
2019-10-28 15:07:19 -0400

    Multiple normalization updates to UI.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = http.createServer(function(req, res)
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      fs.readFile('./public/index.html', null, function(error,data){
+      fs.readFile('../public/index.html', null, function(error,data){
         if (error){
           res.writeHead(404);
           res.write("Uh oh! That webpage doesn't exist! :/ \n");
